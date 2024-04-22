07c719dd023b6de36ff6fdbdd764bcd40b3809c1
LuisaCalcide <96743398+LuisaCalcide@users.noreply.github.com>
2024-04-22 15:56:47 +0200

    Start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          My Three.js app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
