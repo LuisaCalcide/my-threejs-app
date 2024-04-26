fa632b1ac911015f5be28134f5a335c7b7b6e7ad
LuisaCalcide <96743398+LuisaCalcide@users.noreply.github.com>
2024-04-26 17:37:37 +0200

    Fix: error perspective camera

--- src/BasicVrCube/index.js
+++ src/BasicVrCube/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import GLB from '../assets/models/airplane.glb';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 
 let camera, scene, renderer;
@@ -10,44 +10,49 @@ function init() {
     const container = document.createElement( 'div' );
     document.body.appendChild( container );
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.set( 2.5, 1.5, 3.0 );
-
     scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x669FAB );
+
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 100 );
+    camera.position.z = 15;
 
     const gridHelper = new THREE.GridHelper(100,100);
     scene.add(gridHelper);
 
+    //Luci
+    scene.add( new THREE.HemisphereLight( 0xcccccc, 0x999999, 3 ) );
 
-    //creazione di una scena con una perspective camera
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, (window.innerWidth) / window.innerHeight, 0.1, 1000 );
-
-    //Impostazione del rendering dell'app
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    const light = new THREE.DirectionalLight( 0xffffff, 3 );
+    light.position.set( 0, 6, 0 );
+    scene.add( light );
 
     //Aggiungiamo alla scena un cubo(boxGeometry) e creiamo un materiale(material: basic color violet) apposito
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial( { color: 0x874CCC } );
     const cube = new THREE.Mesh( geometry, material );
+    cube.position.y = 0.5;
+    cube.position.x = 3;
     scene.add( cube );
 
-    camera.position.z = 5;
+    const floorGeometry = new THREE.PlaneGeometry( 15, 15 );
+    const floorMaterial = new THREE.MeshBasicMaterial( { color: 0x023020 } )
+    const floor = new THREE.Mesh( floorGeometry, floorMaterial );
+    floor.rotation.x = - Math.PI / 2;
+    floor.receiveShadow = true;
+    scene.add( floor );
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    renderer.toneMappingExposure = 1;
-    container.appendChild( renderer.domElement );
+    renderer.xr.enabled = true;
+    document.body.appendChild( renderer.domElement );
 
     const controls = new OrbitControls( camera, renderer.domElement );
     controls.minDistance = 2;
     controls.maxDistance = 10;
     controls.target.set( 0, 0.5, - 0.2 );
     controls.update();
+
+    document.body.appendChild( VRButton.createButton( renderer ) );
 
     window.addEventListener( 'resize', onWindowResize );
 
